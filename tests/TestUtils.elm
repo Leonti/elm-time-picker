e4fdd63fb34022f2f76857f7ad729362079ae66b
Leonti Bielski <prishelec@gmail.com>
2017-02-11 15:02:18 +1100

    Fix tests

--- tests/TestUtils.elm
+++ tests/TestUtils.elm
@@ -2,21 +2,26 @@ module TestUtils exposing (initialModel)
 
 import TimePicker
 import CalculatedModel exposing (..)
+import Date.Extra.Core exposing (intToMonth)
+import Date.Extra.Create exposing (dateFromFields)
+import Date exposing (Date, hour, minute)
 
 
 initialModel : Bool -> Int -> Int -> InputModel
 initialModel is24Hours hours minutes =
     let
+        date =
+            (dateFromFields 2017 (intToMonth 2) 8 hours minutes 0 0)
+
         model =
             TimePicker.init
                 { is24Hours = is24Hours
-                , hours = hours
-                , minutes = minutes
                 , mainColor = ""
                 }
+                date
     in
         { is24Hours = model.settings.is24Hours
         , mode = model.mode
-        , hoursSelected = model.hoursSelected
-        , minutesSelected = model.minutesSelected
+        , hoursSelected = hour date
+        , minutesSelected = minute date
         }
